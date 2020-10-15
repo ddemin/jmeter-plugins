6d5ece312770ca8e76626910fbbf3621aa9df48a
Dmitry Demin <ddemin@aligntech.com>
2020-10-15 11:54:42 +0300

    [NEW] URL anonymizer and variables filter

--- src/main/java/org/apache/jmeter/visualizers/backend/influxdb2/Influxdb2BackendListenerClient.java
+++ src/main/java/org/apache/jmeter/visualizers/backend/influxdb2/Influxdb2BackendListenerClient.java
@@ -28,9 +28,9 @@ import org.apache.commons.lang3.RandomUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.jmeter.config.Arguments;
 import org.apache.jmeter.samplers.SampleResult;
+import org.apache.jmeter.threads.JMeterContextService;
 import org.apache.jmeter.visualizers.backend.AbstractBackendListenerClient;
 import org.apache.jmeter.visualizers.backend.BackendListenerContext;
-import org.apache.jmeter.visualizers.backend.SamplerMetric;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -50,9 +50,7 @@ import java.util.regex.Pattern;
  */
 public class Influxdb2BackendListenerClient extends AbstractBackendListenerClient implements Runnable {
 
-    private static final Logger log = LoggerFactory.getLogger(Influxdb2BackendListenerClient.class);
-
-    private ConcurrentHashMap<String, SamplerMetric> metricsPerSampler = new ConcurrentHashMap<>();
+    private static final Logger LOG = LoggerFactory.getLogger(Influxdb2BackendListenerClient.class);
 
     private static final String ANNOTATION_MEASUREMENT = "jmeter_events";
     private static final String DEFAULT_MEASUREMENT = "jmeter_transactions";
@@ -71,9 +69,13 @@ public class Influxdb2BackendListenerClient extends AbstractBackendListenerClien
     private static final String EVENTS_SCENARIO = "events_scenario";
     private static final String EVENTS_PROFILE = "events_profile";
     private static final String EVENTS_TAGS = "events_tags";
-    private static final String SAMPLERS_REGEX = "samplers_regex";
-
-    private static final String PATH_ANONYMIZE_REGEXP = "([0-9]+|[0-9a-z-]+-[0-9a-z-]+)";
+    private static final String URL_ANONYMIZE_REGEX = "url_anonymize_regex";
+    private static final String SAMPLERS_REGEX = "samplers_filter_regex";
+    private static final String VARIABLES_REGEX = "variables_filter_regex";
+    private static final String DEFAULT_URL_ANONYMIZE_REGEXP
+            = "\\/([0-9a-zA-Z-]+-[0-9a-zA-Z-]+-[0-9a-zA-Z-]+|[0-9a-z-]+-[0-9a-z-]+|[0-9]+)";
+    private static final String NOT_AVAILABLE = "N/A";
+    private static final int MAX_CHARS_IN_MSG = 32;
 
     static {
         DEFAULT_ARGS.put(INFLUXDB_2_URL, "https://localhost:9999");
@@ -87,7 +89,9 @@ public class Influxdb2BackendListenerClient extends AbstractBackendListenerClien
         DEFAULT_ARGS.put(EVENTS_PROFILE, "Name of load profile");
         DEFAULT_ARGS.put(EVENTS_TAGS, "");
 
+        DEFAULT_ARGS.put(URL_ANONYMIZE_REGEX, DEFAULT_URL_ANONYMIZE_REGEXP);
         DEFAULT_ARGS.put(SAMPLERS_REGEX, ".*");
+        DEFAULT_ARGS.put(VARIABLES_REGEX, "NOTHING");
     }
 
     private final List<Point> measurementsBuffer = new LinkedList<>();
@@ -97,10 +101,13 @@ public class Influxdb2BackendListenerClient extends AbstractBackendListenerClien
     private String launchId;
     private String samplersRegex;
     private Pattern samplersToFilter;
+    private String variablesRegex;
+    private Pattern variablesToFilter;
 
     private ScheduledExecutorService scheduler;
     private ScheduledFuture<?> timerHandle;
     private InfluxDBClient influxDBClient;
+    private String anonymizeRegex;
 
     public Influxdb2BackendListenerClient() {
         super();
@@ -138,7 +145,7 @@ public class Influxdb2BackendListenerClient extends AbstractBackendListenerClien
             throw new RuntimeException(
                     "InfluxDB2 server isn't ready: " +
                             (readiness == null ? "Can't check server readiness" : readiness.getUp())
-                    );
+            );
         }
 
         sendIntervalSec = context.getIntParameter(SEND_INTERVAL_SEC);
@@ -152,14 +159,16 @@ public class Influxdb2BackendListenerClient extends AbstractBackendListenerClien
                         .withKeyValueSeparator('=')
                         .split(context.getParameter(EVENTS_TAGS))
         );
-        eventsTagsMap.put("interval", String.valueOf(sendIntervalSec));
         eventsTagsMap.put("host", InetAddress.getLocalHost().getHostName());
+        eventsTagsMap.put("interval", String.valueOf(sendIntervalSec));
         eventsTagsMap.put("profile", context.getParameter(EVENTS_PROFILE, "N/A"));
         eventsTagsMap.put("scenario", context.getParameter(EVENTS_SCENARIO, "N/A"));
 
+        anonymizeRegex = context.getParameter(URL_ANONYMIZE_REGEX, DEFAULT_URL_ANONYMIZE_REGEXP);
         samplersRegex = context.getParameter(SAMPLERS_REGEX, "");
         samplersToFilter = Pattern.compile(samplersRegex);
-
+        variablesRegex = context.getParameter(VARIABLES_REGEX, "");
+        variablesToFilter = Pattern.compile(variablesRegex);
         sendEvent(true);
 
         scheduler = Executors.newScheduledThreadPool(MAX_POOL_SIZE);
@@ -176,24 +185,26 @@ public class Influxdb2BackendListenerClient extends AbstractBackendListenerClien
     public void teardownTest(BackendListenerContext context) throws Exception {
         try {
             sendEvent(false);
-
             boolean cancelState = timerHandle.cancel(false);
-            log.debug("Canceled state: {}", cancelState);
+            LOG.debug("Canceled state: {}", cancelState);
             scheduler.shutdown();
             try {
                 scheduler.awaitTermination(30, TimeUnit.SECONDS);
             } catch (InterruptedException e) {
-                log.error("Error waiting for end of scheduler");
+                LOG.error("Error waiting for end of scheduler");
                 Thread.currentThread().interrupt();
             }
 
             // Send last set of data before ending
-            log.info("Sending last metrics");
+            LOG.info("Sending last metrics");
             sendMeasurements();
         } finally {
-            influxDBClient.close();
+            try {
+                influxDBClient.close();
+            } finally {
+                super.teardownTest(context);
+            }
         }
-        super.teardownTest(context);
     }
 
     @Override
@@ -204,42 +215,52 @@ public class Influxdb2BackendListenerClient extends AbstractBackendListenerClien
     }
 
     private void saveMeasurement(SampleResult sampleResult) {
-        long timestampNano = sampleResult.getTimeStamp() * 1_000_000 + RandomUtils.nextLong(100000, 999999);
-
-        String trx = sampleResult.getSampleLabel().trim();
-        String code = sampleResult.getResponseCode();
-        String ok = sampleResult.isSuccessful() ? "true" : "false";
         String endpoint;
         String server;
         if (sampleResult.getURL() != null) {
             endpoint = anonymizeUrl(sampleResult.getURL());
-            server = sampleResult.getURL().getHost();
+            sampleResult.getRequestHeaders()
+            server = StringUtils.defaultIfEmpty(sampleResult.getURL().getHost(), NOT_AVAILABLE);
         } else {
-            endpoint = "N/A";
-            server = "N/A";
+            endpoint = NOT_AVAILABLE;
+            server = NOT_AVAILABLE;
         }
 
-        long allThreads = sampleResult.getAllThreads();
-        long duration = sampleResult.getTime();
-        String message = sampleResult.getResponseMessage();
-        String assertion = sampleResult.getFirstAssertionFailureMessage();
-        long inBytes = sampleResult.getBytesAsLong();
-        long outBytes = sampleResult.getSentBytes();
+        String message = StringUtils.defaultIfEmpty(
+                sampleResult.getFirstAssertionFailureMessage(),
+                sampleResult.getResponseMessage()
+        );
+        String code = StringUtils.substring(
+                StringUtils.defaultIfEmpty(sampleResult.getResponseCode(), NOT_AVAILABLE),
+                0,
+                MAX_CHARS_IN_MSG
+        );
 
-        Point point = Point.measurement(DEFAULT_MEASUREMENT)
-                .time(timestampNano, WritePrecision.NS)
-                .addField("bin", inBytes)
-                .addField("bout", outBytes)
-                .addField("duration", duration)
-                .addField("msg", Strings.isNullOrEmpty(assertion) ? message : assertion)
-                .addField("threads", allThreads)
+        Point point = Point
+                .measurement(DEFAULT_MEASUREMENT)
+                .time(
+                        sampleResult.getTimeStamp() * 1_000_000 + RandomUtils.nextLong(100000, 999999),
+                        WritePrecision.NS
+                )
+                .addField("bytes_recv", (float) sampleResult.getBytesAsLong())
+                .addField("bytes_sent", (float) sampleResult.getSentBytes())
+                .addField("tg_threads", (float) sampleResult.getGroupThreads())
+                .addField("time_ms", (float) sampleResult.getTime())
                 .addTag("code", code)
                 .addTag("endpoint", endpoint)
                 .addTag("launch_uuid", launchId)
-                .addTag("name", trx)
-                .addTag("ok", ok)
+                .addTag("passed", sampleResult.isSuccessful() ? "true" : "false")
                 .addTag("server", server)
-                .addTag("code", code);
+                .addTag("trx", sampleResult.getSampleLabel().trim());
+
+        if (!sampleResult.isSuccessful()) {
+            point
+                    .addField(
+                            "msg",
+                            Strings.isNullOrEmpty(message) ? NOT_AVAILABLE : StringUtils.substring(message, 0, MAX_CHARS_IN_MSG)
+                    );
+        }
+
         measurementsBuffer.add(point);
     }
 
@@ -248,7 +269,7 @@ public class Influxdb2BackendListenerClient extends AbstractBackendListenerClien
             try {
                 influxDBClient.getWriteApi().writePoints(measurementsBuffer);
             } catch (Throwable tr) {
-                log.error("Something goes wrong during InfluxDB integration: " + tr.getMessage());
+                LOG.error("Something goes wrong during InfluxDB integration: " + tr.getMessage());
             }
             measurementsBuffer.clear();
         }
@@ -256,21 +277,40 @@ public class Influxdb2BackendListenerClient extends AbstractBackendListenerClien
 
     private void sendEvent(boolean isStartOfTest) throws UnknownHostException {
         eventsTagsMap.put("started", String.valueOf(isStartOfTest));
+        eventsTagsMap.put("launch_uuid", launchId);
 
-        log.debug("Send event with tags: {}", eventsTagsMap);
-        influxDBClient.getWriteApi().writePoint(
-                Point
-                        .measurement(ANNOTATION_MEASUREMENT)
-                        .addField("launch_uuid", launchId)
-                        .addTags(eventsTagsMap)
-                        .time(Instant.now(), WritePrecision.NS)
-        );
+        LOG.debug("Send event with tags: {}", eventsTagsMap);
+        Point eventPoint = Point
+                .measurement(ANNOTATION_MEASUREMENT)
+                .time(Instant.now(), WritePrecision.NS)
+                .addField("total_threads", (float) JMeterContextService.getTotalThreads())
+                .addTags(eventsTagsMap);
+
+        // Add User Variables
+        if (!isStartOfTest) {
+            JMeterContextService.getContext().getVariables().entrySet()
+                    .stream()
+                    .filter(entry -> entry.getValue() != null)
+                    .filter(entry ->
+                            variablesToFilter
+                                    .matcher(String.valueOf(entry.getKey()))
+                                    .find()
+                    )
+                    .sorted(Map.Entry.comparingByKey())
+                    .forEach(entry -> eventPoint.addField(entry.getKey(), String.valueOf(entry.getValue())));
+        }
+
+        influxDBClient.getWriteApi().writePoint(eventPoint);
     }
 
     private String anonymizeUrl(URL url) {
+        if (url.getPath() == null) {
+            return NOT_AVAILABLE;
+        }
         return StringUtils
                 .stripEnd(url.getPath(), "/ ")
-                .replaceAll(PATH_ANONYMIZE_REGEXP, "{ID}");
+                .replaceAll(anonymizeRegex, "/{X}")
+                .replace("//", "/");
     }
 
 }
